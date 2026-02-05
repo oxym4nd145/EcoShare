1bfb5986b8e2f868ce91746a4fbe41fbbf54c1e2
Igor Zwirtes <al.igor.zwirtes@impatech.org.br>
2026-02-05 10:02:32 -0300

    Adiciona arquivos para cada etapa da criação do banco de dados

---
+++ 02_seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO Mensalidade_tipo (id_mensalidade, tipo_mensalidade) VALUES
+(1, 'Sem mensalidade'),
+(2, 'Basica'),
+(3, 'Plus');
+
+INSERT INTO Permissao (id_permissao, tipo_permissao) VALUES
+(1, 'Administrador'),
+(2, 'Moderador'),
+(3, 'Usuario');
+
+INSERT INTO Estado_tipo (id_estado, tipo_estado) VALUES
+(1, 'Novo'),
+(2, 'Seminovo'),
+(3, 'Usado');
+
+INSERT INTO Disponibilidade_tipo (id_disponibilidade, tipo_disponibilidade) VALUES
+(1, 'Disponível'),
+(2, 'Indisponível'),
+(3, 'Em uso'),
+(4, 'Em manutenção');
